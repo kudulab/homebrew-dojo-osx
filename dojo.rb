a54401a974be25f7d1e2c079d7e5251ff80a890b
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-09-19 15:12:32 +0100

    Updated homebrew tap to 0.6.2

--- dojo.rb
+++ dojo.rb
@@ -1,12 +1,13 @@
 class Dojo < Formula
   desc "Containerize your development and operations environment"
   homepage "https://github.com/kudulab/dojo"
-  url "https://github.com/kudulab/dojo/releases/download/0.6.1/dojo_darwin_amd64"
-  sha256 "6d9ba92f3b791d527f034c1a7dba452355ffd78b36769e5c4976e423b167bd99"
+  url "https://github.com/kudulab/dojo/releases/download/0.6.2/dojo_darwin_amd64"
+  sha256 "f54c578f65f94ff803bfb651c13194b0e22710732c5188d4c3eac4ee7bcaef8e"
 
   bottle :unneeded
 
   def install
     bin.install "dojo_darwin_amd64"
+    mv bin/"dojo_darwin_amd64", bin/"dojo"
   end
 end
