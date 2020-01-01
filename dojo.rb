860439308f4fb9bd88750c34f3ae49c53ad06486
Go <go@ai-traders.com>
2020-01-01 12:57:52 +0000

    Updated homebrew tap to 0.7.0

--- dojo.rb
+++ dojo.rb
@@ -1,9 +1,9 @@
 class Dojo < Formula
   desc "Containerize your development and operations environment"
   homepage "https://github.com/kudulab/dojo"
-  url "https://github.com/kudulab/dojo/releases/download/0.6.3/dojo_darwin_amd64"
-  sha256 "a0ad6623270e076c6a8b45321050c70f9d415c80bb23ca0452fc072458c82bd8"
-  version "0.6.3"
+  url "https://github.com/kudulab/dojo/releases/download/0.7.0/dojo_darwin_amd64"
+  sha256 "76aebcbda4cd24cad484fa0893d1e7c115743556676f06defc3851d0a423b512"
+  version "0.7.0"
 
   bottle :unneeded
 
