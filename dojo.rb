4b3fd6d3b46aba4b4eeec01e8d8f832c4ac31922
Go <go@ai-traders.com>
2020-12-07 09:11:38 +0000

    Updated homebrew tap to 0.10.3

--- dojo.rb
+++ dojo.rb
@@ -1,20 +1,20 @@
 class Dojo < Formula
   desc "Containerize your development and operations environment"
   homepage "https://github.com/kudulab/dojo"
-  version "0.10.2"
+  version "0.10.3"
   bottle :unneeded
 
 if OS.mac?
-    url "https://github.com/kudulab/dojo/releases/download/0.10.2/dojo_darwin_amd64"
-    sha256 "d765994d070c39dc0e28b46425488769a27f0c7dd5316188c0479280baa85326"
+    url "https://github.com/kudulab/dojo/releases/download/0.10.3/dojo_darwin_amd64"
+    sha256 "8b4082cf9a0ede5e92a04bf59fc6eec7e4233e62a89c068d9a2865b9de9fe089"
   def install
     bin.install "dojo_darwin_amd64"
     mv bin/"dojo_darwin_amd64", bin/"dojo"
   end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kudulab/dojo/releases/download/0.10.2/dojo_linux_amd64"
-      sha256 "8a85b2804006e835fa11af8ee53e07cff3ce213b3fd57e50cda286e184433828"
+      url "https://github.com/kudulab/dojo/releases/download/0.10.3/dojo_linux_amd64"
+      sha256 "720be2d04e8ff18b3c13d461d061292d7ad9cea118c9b83bb6959d999536aa03"
   def install
     bin.install "dojo_linux_amd64"
     mv bin/"dojo_linux_amd64", bin/"dojo"
